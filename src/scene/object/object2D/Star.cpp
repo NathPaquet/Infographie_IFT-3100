ad021369d0387ee95bf9b25f55f98da59e3ed1f0
Samuel Côté <samuel.cote.15@ulaval.ca>
2024-03-06 14:58:06 -0500

    star object fix

--- src/scene/object/object2D/Star.cpp
+++ src/scene/object/object2D/Star.cpp
@@ -1,6 +1,13 @@
 #include "Star.h"
 
-Star::Star(const glm::vec3 &centerPosition, const float radius = 20.f) {
+const float radiusRatio = 0.6f;
+const int numberOfSection = 10;
+const float rotationPerVertex = 2 * glm::pi<float>() / numberOfSection;
+const glm::mat2x2 rotationMatrix = glm::mat2x2(
+    std::cos(rotationPerVertex), -std::sin(rotationPerVertex),
+    std::sin(rotationPerVertex), std::cos(rotationPerVertex));
+
+Star::Star(const glm::vec3 &centerPosition, const float radius) {
   initMesh(centerPosition, radius);
 }
 
@@ -9,9 +16,33 @@ Star::Star(const glm::vec3 &centerPosition, const glm::vec3 &outerPosition) {
 }
 
 void Star::drawPreview(const glm::vec3 &centerPosition, const float radius) {
+  std::vector<glm::vec3> vertices;
+  // APROXIMATION VALID ONLY IF CAMERA IS LOOKING AT (0,0,1)
+  auto vec1 = glm::vec3(1, 0, 0);
+  auto vec2 = glm::vec3(0, 1, 0);
+
+  auto minRadius = radius * radiusRatio;
+
+  glm::vec2 pointMax = {radius, 0};
+  glm::vec2 pointMin = {minRadius, 0};
+  vertices.push_back(pointMax.x * vec1 + pointMax.y * vec2);
+  for (size_t i = 1; i < numberOfSection; i++) {
+    pointMax = rotationMatrix * pointMax;
+    pointMin = rotationMatrix * pointMin;
+
+    if (i % 2 == 0) {
+      vertices.push_back(pointMax.x * vec1 + pointMax.y * vec2);
+    } else {
+      vertices.push_back(pointMin.x * vec1 + pointMin.y * vec2);
+    }
+  }
+  for (int i = 0; i < numberOfSection; i++) {
+    ofDrawLine(centerPosition + vertices[i], centerPosition + vertices[(i + 1) % numberOfSection]);
+  }
 }
 
 void Star::drawPreview(const glm::vec3 &centerPosition, const glm::vec3 &outerPosition) {
+  drawPreview(centerPosition, glm::length(outerPosition - centerPosition));
 }
 
 void Star::initMesh(const glm::vec3 &centerPosition, const float radius) {
@@ -20,15 +51,15 @@ void Star::initMesh(const glm::vec3 &centerPosition, const float radius) {
   auto vec1 = glm::vec3(1, 0, 0);
   auto vec2 = glm::vec3(0, 1, 0);
 
-  auto minRadius = radius - this->diffRadius;
+  auto minRadius = radius * radiusRatio;
   vertices.push_back({0, 0, 0});
 
   glm::vec2 pointMax = {radius, 0};
   glm::vec2 pointMin = {minRadius, 0};
   vertices.push_back(pointMax.x * vec1 + pointMax.y * vec2);
-  for (size_t i = 1; i < this->numberOfSection; i++) {
-    pointMax = this->rotationMatrix * pointMax;
-    pointMin = this->rotationMatrix * pointMin;
+  for (size_t i = 1; i < numberOfSection; i++) {
+    pointMax = rotationMatrix * pointMax;
+    pointMin = rotationMatrix * pointMin;
 
     if (i % 2 == 0) {
       vertices.push_back(pointMax.x * vec1 + pointMax.y * vec2);
@@ -39,9 +70,12 @@ void Star::initMesh(const glm::vec3 &centerPosition, const float radius) {
 
   ofMesh mesh = ofMesh(OF_PRIMITIVE_TRIANGLES, vertices);
 
-  for (int i = 0; i < this->numberOfSection; i++) {
+  for (int i = 0; i < numberOfSection; i++) {
     mesh.addIndex(i + 1);
-    mesh.addIndex((i + 1) % this->numberOfSection + 1);
+    mesh.addIndex((i + 1) % numberOfSection + 1);
     mesh.addIndex(0);
   }
+
+  this->primitive = std::make_unique<of3dPrimitive>(of3dPrimitive(mesh));
+  this->primitive->setPosition(centerPosition);
 }
--- src/scene/object/object2D/Star.h
+++ src/scene/object/object2D/Star.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "object/sceneObject.h"
 
-class Star : public SceneObject{
+class Star : public SceneObject {
 public:
   Star(const glm::vec3 &centerPosition, const float radius = 20.f);
   Star(const glm::vec3 &centerPosition, const glm::vec3 &outerPosition);
@@ -11,10 +11,4 @@ public:
 
 private:
   void initMesh(const glm::vec3 &centerPosition, const float radius);
-  const float diffRadius = 10.f;
-  const int numberOfSection = 10;
-  const float rotationPerVertex = 2 * glm::pi<float>() / numberOfSection;
-  const glm::mat2x2 rotationMatrix = glm::mat2x2(
-      std::cos(rotationPerVertex), -std::sin(rotationPerVertex),
-      std::sin(rotationPerVertex), std::cos(rotationPerVertex));
 };
