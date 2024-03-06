a996345e7eb3a040239fecb3a53e2e25880babd7
Nathan Paquet <NAPAQ83@ulaval.ca>
2024-03-06 13:05:58 -0500

    feat: Add size property to 3D model

--- src/scene/object/object3D/Model3D.cpp
+++ src/scene/object/object3D/Model3D.cpp
@@ -5,7 +5,9 @@
 constexpr float DEFAULT_SIZE = 40.0f;
 
 Model3D::Model3D(const Ray &ray, const float &distance, const string &modelPath) {
+  this->addProperty<float>(PROPERTY_ID::SIZE, DEFAULT_SIZE);
   // TODO : find a way not to load mesh in this section !
+
   ofxAssimpModelLoader model;
   model.load(modelPath, ofxAssimpModelLoader::OPTIMIZE_DEFAULT);
   auto position = ray.getOrigin() + ray.getDirection() * distance;
@@ -30,8 +32,20 @@ Model3D::Model3D(const Ray &ray, const float &distance, const string &modelPath)
       yExtremes.second->y - yExtremes.first->y,
       zExtremes.second->z - zExtremes.first->z);
 
-  float scaleFactor = DEFAULT_SIZE / max(boundingBox.z, max(boundingBox.x, boundingBox.y));
+  this->initialScaleFactor = DEFAULT_SIZE / max(boundingBox.z, max(boundingBox.x, boundingBox.y));
 
   this->primitive->setPosition(position);
-  this->primitive->setScale(scaleFactor);
+  this->primitive->setScale(this->initialScaleFactor);
+}
+
+void Model3D::updateProperties() {
+  SceneObject::updateProperties();
+  if (this->properties.at(PROPERTY_ID::SIZE)->isValueChanged()) {
+    this->set_size(this->getPropertyValue<float>(PROPERTY_ID::SIZE));
+    this->properties.at(PROPERTY_ID::SIZE)->setChanged(false);
+  }
+}
+
+void Model3D::set_size(const float size) {
+  this->primitive.get()->setScale((size / DEFAULT_SIZE) * this->initialScaleFactor);
 }
--- src/scene/object/object3D/Model3D.h
+++ src/scene/object/object3D/Model3D.h
@@ -6,4 +6,9 @@
 class Model3D : public SceneObject {
 public:
   Model3D(const Ray &ray, const float &distance, const string &modelPath);
+
+private:
+  float initialScaleFactor;
+  void set_size(const float size);
+  void updateProperties() override;
 };
