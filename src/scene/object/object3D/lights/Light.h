75bf281212b1a8b839b4217f57ce3c74f72effaf
Nathan Paquet <NAPAQ83@ulaval.ca>
2024-04-23 17:40:16 -0400

    refactor: fix light draw

--- src/scene/object/object3D/lights/Light.h
+++ src/scene/object/object3D/lights/Light.h
@@ -5,6 +5,7 @@ class Light : public Object3D {
 public:
   Light(const glm::vec3 &position);
   ~Light();
+  void draw(bool isSelected, bool isBoundingBoxEnable, bool isObjectAxisEnable) const override;
 
 private:
   const float proportionToDraw = 3 / 16.f;
