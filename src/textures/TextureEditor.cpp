a3d758af71ea4346608d512cd8f6912028e4e2a5
Samuel Côté <samuel.cote.15@ulaval.ca>
2024-05-02 15:05:17 -0400

    rebase fix

--- src/textures/TextureEditor.cpp
+++ src/textures/TextureEditor.cpp
@@ -175,7 +175,8 @@ void TextureEditor::drawMaterialProperties() {
   ImGui::SetNextItemWidth(200.f);
   if (ImGui::SliderFloat("Brightness", &brightness, 0.f, 1.f)) {
     TextureRepository::setBrightness(currentTexture->packId, brightness);
-    
+  }
+
   ImGui::SetNextItemWidth(200.f);
   if (ImGui::Checkbox(has_reinhard_tone_mapping ? "Reinhard Enable" : "ACES Filmic Enable", &has_reinhard_tone_mapping)) {
     TextureRepository::setReinhardToneMapping(currentTexture->packId, has_reinhard_tone_mapping);
